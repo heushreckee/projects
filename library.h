744529e059f4e22bebae3bd5c793f00f8dba065b
heushreckee <98458059+heushreckee@users.noreply.github.com>
2022-06-18 13:07:03 +0300

    Create library.h

---
+++ library.h
@@ -0,0 +1,17 @@
+
+#ifndef OOP_ASSIGNMENTS_LIBRARY_H
+#define OOP_ASSIGNMENTS_LIBRARY_H
+namespace geometry {
+
+class Vector;
+class IShape;
+class Point;
+class Segment;
+class Ray;
+class Line;
+class Circle;
+class Polygon;
+
+}  // namespace geometry
+
+#endif  // OOP_ASSIGNMENTS_LIBRARY_H
